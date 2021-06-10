fc54c89fea7dc0862384175cc2cdccc809bfefbd
Sávio Muniz <savio.muniz@vtex.com.br>
2021-06-10 16:30:09 -0300

    demo

--- src/module/server.ts
+++ src/module/server.ts
@@ -20,6 +20,9 @@ export class WebServer {
     this.router.get('/user', userHandler)
     this.router.get('/secret', secretHandler)
     this.router.get('/healthcheck', healthHandler)
+    this.router.get('/demo', (ctx) => {
+      ctx.body = 'testing service'
+    })
     this.router.get('/(.*)', (ctx) => {
       ctx.body = 'Default route!'
     })
